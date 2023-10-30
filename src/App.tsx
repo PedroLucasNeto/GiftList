55714f327b93b67943947141c193a5fe64ede463
Pedro Lucas <pedrolukasneto@gmail.com>
2023-10-30 20:50:03 -0300

    feat: list services

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import "./App.css";
-import Home from "./Pages/Home";
-import NotFound from "./Pages/NotFound";
-import NavBar from "./components/NavBar";
-import Footer from "./components/Footer";
-import { Contact } from "./components/Contact";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import './App.css';
+import Home from './Pages/Home';
+import NotFound from './Pages/NotFound';
+import NavBar from './components/NavBar';
+import Footer from './components/Footer';
+import { Contact } from './components/Contact';
+import ServiceDetails from './components/ServiceDetails';
 
 function App() {
   return (
@@ -12,12 +13,13 @@ function App() {
       <BrowserRouter>
         <NavBar />
         <Routes>
-          <Route path="*" element={<NotFound />} />
+          <Route path='*' element={<NotFound />} />
           <Route index element={<Home />} />
-          <Route path="/home" element={<Home />} />
-          <Route path="/*" element={<NotFound />} />
-          <Route path="notFound" element={<NotFound />} />
-          <Route path="/contact" element={<Contact />} />
+          <Route path='/home' element={<Home />} />
+          <Route path='/*' element={<NotFound />} />
+          <Route path='notFound' element={<NotFound />} />
+          <Route path='/contact' element={<Contact />} />
+          <Route path='/services' element={<ServiceDetails />} />
         </Routes>
         <Footer />
       </BrowserRouter>
