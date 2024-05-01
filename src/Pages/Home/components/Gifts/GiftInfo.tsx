ea5673f2332e4a8865b7cca186de44932d65f56a
Pedro Lucas <pedrolukasneto@gmail.com>
2024-04-30 22:37:14 -0300

    fix: removing pix info

--- src/Pages/Home/components/Gifts/GiftInfo.tsx
+++ src/Pages/Home/components/Gifts/GiftInfo.tsx
@@ -1,29 +1,23 @@
-import { ShoppingBagIcon } from "@heroicons/react/24/outline";
+import { ShoppingBagIcon } from '@heroicons/react/24/outline';
 
 export const GiftInfo = () => {
   return (
-    <div>
-      <h1 className="font-hurricane mb-0 text-5 text-darkblue font-normal">
+    <div className='mb-8'>
+      <h1 className='font-hurricane mb-0 text-5 text-darkblue font-normal'>
         Lista de Presentes
       </h1>
-      <div className="text-left self-center text-lg  mb-16 ">
+      <div className='text-left self-center text-lg  mb-16 '>
         <p>
           Familia e Amigos, sintam-se a vontade caso queiram nos presentear!
-          <br />
-          Além da lista de presentes vou deixar nossa chave PIX caso alguem
-          prefira utilizar.
         </p>
-        <p>Erick Henrique</p>
-        <p>PIX: 83 99999-4788 </p>
-        <p>QR CODE</p>
-        <div className="relative w-full">
-          <button
-            className="rounded-md bg-darkbrown absolute right-0 bottom-0 p-2 text-white 
-    hover:bg-darknude hover:text-darkbrown flex flex-row items-center gap-2"
-          >
-            <ShoppingBagIcon className="w-5" /> <span>Carrinho</span>
-          </button>
-        </div>
+      </div>
+      <div className='relative w-full'>
+        <button
+          className='rounded-md bg-darkbrown absolute right-0 bottom-0 p-2 text-white 
+    hover:bg-darknude hover:text-darkbrown flex flex-row items-center gap-2'
+        >
+          <ShoppingBagIcon className='w-5' /> <span>Carrinho</span>
+        </button>
       </div>
     </div>
   );
